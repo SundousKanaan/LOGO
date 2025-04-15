990577766a662a8c7f91fbf189c003492ff22158
SundousKanaan <94317411+SundousKanaan@users.noreply.github.com>
2025-04-15 12:16:53 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import Navbar from "./components/Navbar/Navbar";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
