9bc2537f6f278b215faf29cefc4cac462c843c2b
SundousKanaan <94317411+SundousKanaan@users.noreply.github.com>
2025-04-15 14:59:46 +0200

    update: install Chakra UI

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,19 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./index.css";
 import App from "./app";
 import Login from "./pages/Login/Login";
+import { Provider } from "./components/ui/provider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/login" element={<Login />} />
-        {/* Redirect to app.jsx with open Dashboard if no match */}
-        <Route path="/*" element={<Navigate to="/" />} />{" "}
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <Provider>
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/login" element={<Login />} />
+          {/* Redirect to app.jsx with open Dashboard if no match */}
+          <Route path="/*" element={<Navigate to="/" />} />{" "}
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>
 );
