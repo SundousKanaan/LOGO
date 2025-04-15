b66e01a461018517702b35141f551bb75d7a3f95
SundousKanaan <94317411+SundousKanaan@users.noreply.github.com>
2025-04-15 13:02:13 +0200

    update: router wrapper

--- src/pages/Reviews/Reviews.jsx
+++ src/pages/Reviews/Reviews.jsx
@@ -1,13 +1,7 @@
-import Navbar from "../../components/Navbar/Navbar";
-import Header from "../../components/Header/Header";
-
 export default function reviews() {
   return (
     <>
-      <Navbar />
-      <section className="pageContainer">
-        <Header />
-      </section>
+      <h2>Reviews page is open</h2>
     </>
   );
 }
